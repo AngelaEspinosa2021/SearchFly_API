e7f593181140e1a458c8b6d52dcd6f2f933c7852
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-11-24 11:56:46 -0500

    Creación de las tablas Flight y Transport

---
+++ SearchFly_API/Models/Flight.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SearchFly_API.Models
+{
+    public class Flight
+    {
+        public int Id { get; set; }
+
+        public string DepartureStation { get; set; }
+
+        public string ArrivalStation { get; set; }
+
+        public DateTime DepartureDate { get; set; }
+
+        public Decimal Price { get; set; }
+
+        public string Currency { get; set; }
+    }
+}
---
+++ SearchFly_API/Models/Transport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SearchFly_API.Models
+{
+    public class Transport
+    {
+        public int Id { get; set; }
+
+        public string FlightNumber { get; set; }
+
+    }
+}
