56b446e8f5fcefc84748d2339b07145e912a06f6
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-11-24 12:41:09 -0500

    creacion ApplicationDbContext dentro de la carpeta Data.

---
+++ SearchFly_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SearchFly_API.Models;
+
+namespace SearchFly_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Transport> Transports { get; set; }
+    }
+}
