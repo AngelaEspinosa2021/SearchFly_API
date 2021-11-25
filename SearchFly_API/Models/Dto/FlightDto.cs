20878b8843c2c593061685c69a29debc51faf792
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-11-25 12:15:42 -0500

    Implementación Interfaz para Flights y creacion de FlightDto

---
+++ SearchFly_API/Models/Dto/FlightDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SearchFly_API.Models.Dto
+{
+    public class FlightDto
+    {
+        public int Id { get; set; }
+        
+        public string DepartureStation { get; set; }
+        
+        public string ArrivalStation { get; set; }
+        
+        public DateTime DepartureDate { get; set; }
+
+        public int TransportRefId { get; set; }
+
+        public Transport Transport { get; set; }   
+        
+        public decimal Price { get; set; }    
+        
+        public string Currency { get; set; }
+
+    }
+}
