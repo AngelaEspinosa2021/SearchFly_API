c362bdc07d0a4bec9d71ea58ddd18af5fab8b0ba
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-11-25 12:18:17 -0500

    Implementación Interfaz para Transport y creacion de TransportDto

---
+++ SearchFly_API/Models/Dto/TransportDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SearchFly_API.Models.Dto
+{
+    public class TransportDto
+    {
+        public int Id { get; set; }
+
+        public string FlightNumber { get; set; }
+
+        public ICollection<Flight> Flight { get; set; }
+    }
+}
