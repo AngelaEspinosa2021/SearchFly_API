69819bd7ba01ae6a2b6988679c45c82d2d2331a3
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-11-25 14:14:29 -0500

    Implementación de IFlightRepository dentro la clase FlightRepository

--- SearchFly_API/Repository/FlightRepository.cs
+++ SearchFly_API/Repository/FlightRepository.cs
@@ -1,11 +1,31 @@
-﻿using System;
+﻿using SearchFly_API.Models.Dto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SearchFly_API.Repository
 {
-    public class FlightRepository
+    public class FlightRepository : IFlightRepository
     {
+        public Task<FlightDto> CreateUpdate(FlightDto flightDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteFlight(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FlightDto> GetFlightById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<FlightDto>> GetFlights()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
