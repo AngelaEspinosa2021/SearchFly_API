589a0851959c791dacbbe36af03f59c05faece4d
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-11-25 13:42:19 -0500

    Definion de las propiedades de la Clase ResponseDto.

--- SearchFly_API/Models/Dto/ResponseDto.cs
+++ SearchFly_API/Models/Dto/ResponseDto.cs
@@ -7,5 +7,12 @@ namespace SearchFly_API.Models.Dto
 {
     public class ResponseDto
     {
+        public bool IsSucess { get; set; }
+
+        public object Result { get; set; }
+
+        public string DisplayMessage { get; set; }
+
+        public List<string> ErrorMessage { get; set; }
     }
 }
