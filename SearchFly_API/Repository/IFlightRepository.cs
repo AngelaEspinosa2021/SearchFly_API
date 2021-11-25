b12597e4dd867674f5b9a40a4efed0da4f31b226
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-11-25 13:53:17 -0500

    Definición Métodos Básicos IFlightRepository.

--- SearchFly_API/Repository/IFlightRepository.cs
+++ SearchFly_API/Repository/IFlightRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SearchFly_API.Models.Dto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,12 @@ namespace SearchFly_API.Repository
 {
     public interface IFlightRepository
     {
+        Task<List<FlightDto>> GetFlights();
+
+        Task<FlightDto> GetFlightById(int id);
+
+        Task<FlightDto> CreateUpdate(FlightDto flightDto);
+
+        Task<bool> DeleteFlight(int id);
     }
 }
