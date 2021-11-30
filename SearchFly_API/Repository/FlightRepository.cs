37f658e69593773a756827567f89866a2ae0216b
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-11-30 12:11:33 -0500

    creación de metodos privados createAllShedulesList y createShedulesAvailableList en el FLightRepository para mejorar el funcionamiento de FlightRepository

--- SearchFly_API/Repository/FlightRepository.cs
+++ SearchFly_API/Repository/FlightRepository.cs
@@ -34,9 +34,21 @@ namespace SearchFly_API.Repository
         public async Task<List<FlightDto>> SearchFlights(string departureStation, string arrivalStation, DateTime departureDate)
         {
             List<Flight> listFlights = await _db.Flights.ToListAsync();
-            List<Flight> finalList = new List<Flight>();
+            List<Flight> finalList = new List<Flight>(); 
             List<DateTime> shedulesAvailable = new List<DateTime>();
-            List<DateTime> filterList = new List<DateTime>();
+
+            shedulesAvailable = createShedulesAvailableList(departureStation, arrivalStation, departureDate, listFlights);
+                    
+            foreach (var list in shedulesAvailable)
+            {
+                finalList.Add(new Flight {DepartureStation = departureStation, ArrivalStation = arrivalStation, DepartureDate = list});
+            }
+
+            return _mapper.Map<List<FlightDto>>(finalList);
+        }
+
+        private List<DateTime> createAllShedulesList(DateTime departureDate)
+        {
             List<DateTime> allShedules = new List<DateTime>();
             List<DateTime> allShedulesList = new List<DateTime>();
             allShedules.Add(Convert.ToDateTime("2021-01-01T00:00:00.0000000"));
@@ -63,13 +75,24 @@ namespace SearchFly_API.Repository
             allShedules.Add(Convert.ToDateTime("2021-01-01T03:00:00.0000000"));
             allShedules.Add(Convert.ToDateTime("2021-01-01T02:00:00.0000000"));
             allShedules.Add(Convert.ToDateTime("2021-01-01T01:00:00.0000000"));
-
+                        
             TimeSpan times = new TimeSpan();
             foreach (var list in allShedules)
             {
                 times = TimeSpan.FromHours(list.Hour);
                 allShedulesList.Add(departureDate.Date + times);
             }
+            return (allShedulesList);
+        }
+
+        private List<DateTime> createShedulesAvailableList(string departureStation, string arrivalStation, DateTime departureDate , List<Flight> listFlights)
+        {
+           
+            List<DateTime> allShedulesList = new List<DateTime>();
+            List<DateTime> filterList = new List<DateTime>();
+            List<DateTime> shedulesAvailable = new List<DateTime>();
+
+            allShedulesList = createAllShedulesList(departureDate);
 
             foreach (var flight in listFlights)
             {
@@ -80,18 +103,15 @@ namespace SearchFly_API.Repository
                 {
                     filterList.Add(flight.DepartureDate);
                 }
-                
+
             }
 
             shedulesAvailable = (from a in allShedulesList
                                  where !filterList.Any(x => x.Hour == a.Hour)
                                  select a).ToList();
-            
-            foreach (var list in shedulesAvailable)
-            {
-                finalList.Add(new Flight {DepartureStation = departureStation, ArrivalStation = arrivalStation, DepartureDate = list});
-            }
-            return _mapper.Map<List<FlightDto>>(finalList);
+
+            return (shedulesAvailable);
+
         }
 
     }
